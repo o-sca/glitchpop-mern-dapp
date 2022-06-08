c81e123beacd50efc01fd0fe70bc86c72a7a5ea5
Oscar Zhu <oscar@yvrmail.net>
2022-06-08 01:41:34 -0700

    removed alias

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
-import Header from "@components/Header";
-import Footer from "@components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Main from "./views/Main";
 
-
-export default class App extends Component {
-  render() {
-    return (
-      <>
-      <Header />
-      <Main />
-      <Footer />
-      </>
-    )
-  }
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Main />
+    <Footer />
+    </>
+  );
 };
+
+export default App;
