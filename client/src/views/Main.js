774fb2039f5bc58b68feef0a8be2259753faa99b
Oscar Zhu <oscar@yvrmail.net>
2022-06-08 03:30:53 -0700

    added headers folder

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,10 +1,11 @@
 import aboutUsImg from "../assets/images/genesis-art.svg";
+import aboutUsHeader from "../assets/headers/About-Header.svg";
 
 const Main = () => {
   return (
     <main>
       <div className="about-us">
-        
+
       </div>
     </main>
   );
