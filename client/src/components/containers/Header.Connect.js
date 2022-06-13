0c2278dc23df127813bb383e4fad43652fdeb056
Oscar Zhu <oscar@yvrmail.net>
2022-06-13 03:32:16 -0700

    created Headers Connect

---
+++ client/src/components/containers/Header.Connect.js
@@ -0,0 +1,26 @@
+import { connect, disconnect } from "../web3/Wallet.handler";
+
+const Connect = () => {
+  const connectStyle = {
+    display: "block"
+  };
+
+  const disconnectStyle = {
+    display: "none"
+  };
+
+  return (
+    <div className="connect-container">
+      <button className="connect-btn">
+        <div id="disconnect" onClick={disconnect} style={disconnectStyle}>
+          <span>DISCONNECT</span>
+        </div>
+        <div id="connect" onClick={connect} style={connectStyle}>
+          <span>CONNECT</span>
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default Connect;
--- client/src/components/containers/Header.Socials.js
+++ client/src/components/containers/Header.Socials.js
@@ -1,23 +1,14 @@
 import twitter from "../../assets/buttons/twitter-button.svg";
 import discord from "../../assets/buttons/Discord Button.svg";
 import opensea from "../../assets/buttons/OpenSea.svg";
-
+import Connect from "./Header.Connect";
 import {
   twitterEvent,
   discordEvent,
   openseaEvent,
 } from "../buttons/Socials.handler";
-import { connect, disconnect } from "../web3/Wallet.handler";
 
 const Socials = () => {
-  const connectStyle = {
-    display: "block"
-  };
-
-  const disconnectStyle = {
-    display: "none"
-  };
-
   return (
     <div className="socials">
       <button>
@@ -29,16 +20,7 @@ const Socials = () => {
       <button>
         <img src={discord} alt="discord" id="discord" onClick={discordEvent} />
       </button>
-      <div className="connect-container">
-        <button className="connect-btn">
-          <div id="disconnect" onClick={disconnect} style={disconnectStyle}>
-            <span>DISCONNECT</span>
-          </div>
-          <div id="connect" onClick={connect} style={connectStyle}>
-            <span>CONNECT</span>
-          </div>
-        </button>
-      </div>
+      <Connect />
     </div>
   );
 };
