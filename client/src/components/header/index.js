199b40f1a4b4f626de329a5451a77a2d5aa0caff
Oscar Zhu <oscar@yvrmail.net>
2022-06-23 18:45:19 -0700

    redone desktop header css

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -6,17 +6,52 @@ import avatar from "../../assets/images/avatar.svg";
 import logo from "../../assets/images/logo.svg";
 import "../../assets/styles/header.css";
 
+import { useState, useLayoutEffect } from "react";
+
 const Header = () => {
+  const [isMobile, setMobile] = useState(false);
+
+  const onResize = () => {
+    const isMobile = window.innerWidth < 768;
+    setMobile(isMobile);
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener("resize", onResize);
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
   return (
     <header>
-      <ConnectButton />
-      <img src={logo} alt="logo" className="logo" />
-      <MintButton />
-      <MintEdit />
-      
-        <img src={avatar} alt="avatar" className="avatar" />
-        <Socials />
-      
+      {isMobile ? (
+        <>
+          <ConnectButton />
+          <img src={logo} alt="logo" className="logo" />
+          <MintButton />
+          <MintEdit />
+          <div className="mobile">
+            <img src={avatar} alt="avatar" className="avatar" />
+            <Socials />
+          </div>
+        </>
+      ) : (
+        <>
+          <img src={avatar} alt="avatar" className="avatar" />
+          <div className="container">
+            <div className="upper-btn-item">
+              <Socials />
+              <ConnectButton />
+            </div>
+            <img src={logo} alt="logo" className="logo" />
+            <div className="lower-btn-item">
+              <MintButton />
+              <MintEdit />
+            </div>
+          </div>
+        </>
+      )}
     </header>
   );
 };
